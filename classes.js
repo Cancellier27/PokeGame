ae80133aefa2f04e001965bbef6c7750453d2a57
Filipe Cancellier da Costa <filipecancelliercosta@gmail.com>
2022-03-29 18:09:30 +0100

    adding rotation to enemys attacks

--- classes.js
+++ classes.js
@@ -67,6 +67,9 @@ class Sprite {
     let healthBar = ".enemyHp"
     if (this.isEnemy) healthBar = ".ourHp"
 
+    let rotation = 1
+    if (this.isEnemy) rotation = -2.2
+
     this.health -= attack.damage
 
     switch (attack.name) {
@@ -84,7 +87,7 @@ class Sprite {
             hold: 10
           },
           animate: true,
-          rotation: 1
+          rotation
         })
 
         renderSprites.splice(1, 0, fireball)
